d06f3d40dae2b547dc3d8a3fb3f8a7eb72990d2f
rlopezpau <r.lopezpau@gmail.com>
2025-11-06 11:11:41 +0100

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,18 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>Mapa Económico</title>
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+        <link 
+          rel="stylesheet" 
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossOrigin=""
+        />
+        <style>{`
+          .leaflet-container {
+            height: 100vh;
+            width: 100%;
+          }
+        `}</style>
       </Head>
       {children}
     </>
